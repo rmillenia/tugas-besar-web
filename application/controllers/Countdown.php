9a4550e459f5301423f2ac84ab46d96525df7be8
rmillenia <35435291+rmillenia@users.noreply.github.com>
2018-06-03 13:35:53 +0700

    edit countdown

--- application/controllers/Countdown.php
+++ application/controllers/Countdown.php
@@ -5,10 +5,6 @@ class  Countdown extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('v_countdown');
-	}
-
- 	public function lihat_countdown(){
  		$this->load->model('mcountdown');
  		$result['timer'] = $this->mcountdown->select_time();
  		$this->load->view('v_timer', $result);
--- application/views/v_timer.php
+++ application/views/v_timer.php
@@ -56,7 +56,7 @@ function refresh()
 {
     setTimeout(function()
     {
-        $('#refreshTime').innerHTML.load('<?php echo base_url();?>CountDown/lihat_countdown');
+        $('#refreshTime').innerHTML.load('<?php echo base_url();?>CountDown');
     }, 1000);
 };
 
