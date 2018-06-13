ab2942af5599bf283d53124de034c721962c6858
rmillenia <35435291+rmillenia@users.noreply.github.com>
2018-06-13 20:33:36 +0700

    header admin edit

--- application/views/user/header.php
+++ application/views/user/header.php
@@ -11,7 +11,7 @@
 				          <li><a href="#schedule">All Event</a></li>
 				          <li class="menu-has-children"><a href="">Account</a>
 				            <ul>
-				              <li><a href="#" data-toggle="modal" data-target="#modal1" class="btn btn-lg btn-success smoothScroll wow fadeInUp" data-wow-delay="1.0s">Login</a></li>
+				              <li><a href="generic.html">Login</a></li>
 				              <li><a href="elements.html">Register</a></li>
 				            </ul>
 				          </li>
@@ -20,50 +20,4 @@
 				      </nav><!-- #nav-menu-container -->		    		
 			    	</div>
 			    </div>
-			  </header>
-			  <!-- #header -->
-			  <!-- modal -->
-<div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-      <div class="modal-content modal-popup">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h2 class="modal-title">Login</h2>
-        </div>
-        <form action="#" method="post">
-          <input name="username" type="text" class="form-control" id="username" placeholder="Username">
-          <input name="password" type="password" class="form-control" id="password" placeholder="Password">
-          <input name="submit" type="submit" class="form-control" id="submit" value="Submit">
-        </form>
-         <font color="white"><h4>Don't have any account?</h4>
-          <a href="#" data-toggle="modal" data-target="#modal2" class="smoothScroll wow fadeInUp" data-wow-delay="1.0s" style="text-decoration: none">Register</a>
-      </div>
-  </div>
-</div>
-
-
-<div class="modal fade" id="modal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-      <div class="modal-content modal-popup">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h2 class="modal-title">Register</h2>
-        </div>
-        <form action="#" method="post">
-           <div class="row">
-            <div class="col-lg-6">
-          <input name="name" type="text" class="form-control" id="name" placeholder="Full Name">
-          <input name="address" id="address" class="form-control" placeholder="Address">
-          <input name="hp" type="text" class="form-control" id="hp" placeholder="Phone Number">
-        </div>
-        <div class="col-lg-6">
-          <input name="email" type="email" class="form-control" id="email" placeholder="E-Mail">
-          <input name="username" type="text" class="form-control" id="username" placeholder="Username">
-          <input name="password" type="password" class="form-control" id="password" placeholder="Password">
-        </div>
-      </div>
-       <input name="submit" type="submit" class="form-control btn-lg" id="submit" value="Submit">
-        </form>
-      </div>
-  </div>
-</div>
+			  </header><!-- #header -->
