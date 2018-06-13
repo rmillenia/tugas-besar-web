9443f4a3386ae329057bf98a94f927c1c961c2e8
rmillenia <35435291+rmillenia@users.noreply.github.com>
2018-06-13 08:57:43 +0700

    edit tampilan event artist

--- application/views/admin/artistEvent.php
+++ application/views/admin/artistEvent.php
@@ -40,46 +40,36 @@
                     </ol>
                 </div>
             </div>
-
-	<div class="container-fluid">
-		<div class="card">
-            <div class="card-body">
-           		<div class="form-group text-right">
+            <div class="form-group text-right">
 					<button data-toggle="modal" data-target="#tambah-data" class="btn btn-outline-secondary"><span class="fa fa-plus"></span> Add</button>
 				</div>
-                <div class="table-responsive">
-                    <table class="table" id="example">	
-						<thead>
-							<tr>
-								<!--<th>ID</th> -->
-								<th>Name</th>
-								<th>Gender</th>
-								<th>Birth Date</th>
-								<th>Picture</th>
-								<th>  </th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php foreach ($eventartist_list as $key){ ?>
-							<tr>
-							<!--<td><?php echo $key->id ; ?></td> -->
-							<td><?php echo $key->artist ; ?></td>
-							<td><?php echo $key->gender ; ?></td>
-							<td><?php echo $key->birthdate ; ?></td>
-							<td><img src="<?php echo base_url();?>assets/imgEvent/<?php echo $key->picture;?>" class="img-thumbnail" width="50"></td>
-							<td>
-								<a href="javascript:void(0);" data-id="<?php echo $key->idArtist ; ?>" data-name="<?php echo $key->artist ; ?>" data-gender="<?php echo $key->gender ; ?>" data-birth="<?php echo $key->birthdate ; ?>" data-pic="<?php echo $key->picture ; ?>" data-toggle="modal" data-target="#edit-data">
-                            	<button  data-toggle="modal" data-target="#ubah-data" class="btn btn-secondary"><span class="fa fa-edit"></span></button></a>
-								<a href="<?php echo site_url()?>/EventName/delete/<?php echo $key->idArtist; ?>" class="btn btn-danger" onclick="return confirm('Are you sure to delete this data permanently?'); "><span class="fa fa-trash"></span></a>
-							</td>
-							</tr>
-							<?php } ?>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-	</div>
+	<center>
+	<div class="row">
+    <?php foreach ($eventartist_list as $key){ ?>
+    <div class="col-lg-4 col-xlg-3 col-md-5">
+		<div class="card" style="width: 275; height: 250">
+            	<center><img src="<?php echo base_url()?>assets/imgEvent/<?php echo $key->picture;?>" class="img-circle" width="100" height="100" />
+            <h4 class="card-title"><?php echo $key->artist ; ?></h4>
+            <div class="card-body">
+            <div class="row text-center justify-content-md-center">
+            	<div class="col-4">
+            		<a href="javascript:void(0);" data-id="<?php echo $key->idArtist ; ?>" data-name="<?php echo $key->artist ; ?>" data-gender="<?php echo $key->gender ; ?>" data-birth="<?php echo $key->birthdate ; ?>" data-pic="<?php echo $key->picture ; ?>" data-toggle="modal" data-target="#edit-data">
+                    <button  data-toggle="modal" data-target="#ubah-data" class="btn btn-secondary"><span class="fa fa-edit"></span></button></a>
+                </div>
+                <div class="col-4">
+                	<a href="<?php echo site_url()?>/EventArtist/delete/<?php echo $key->idArtist; ?>" class="btn btn-danger" onclick="return confirm('Are you sure to delete this data permanently?'); "><span class="fa fa-trash"></span></a>
+                </div>
+            </div>
+            
+        </div>
+    </center>
+</div>
+</div>
+
+<?php } ?>
+</div>
+</center>
+
 	<!-- Modal Tambah -->
 	<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="tambah-data" class="modal fade-in">
 	    <div class="modal-dialog">
@@ -91,25 +81,25 @@
 		            <div class="modal-body">
 		            	<div class="container-fluid">
 		                    <div class="form-group">
-		                        <label class="col-lg-2 col-sm-2 control-label">Name</label>
+		                        <label class="col-lg-4 col-sm-4 control-label">Name</label>
 		                        <div class="col-lg-12">
-		                            <input type="text" class="form-control" name="name" placeholder="Input Event Name" required>
+		                            <input type="text" class="form-control" name="name" placeholder="Input Artist Name" required>
 		                        </div>
 		                    </div>
 		                    <div class="form-group">
-		                        <label class="col-lg-2 col-sm-2 control-label">Gender</label>
+		                        <label class="col-lg-4 col-sm-4 control-label">Gender</label>
 		                        <div class="col-lg-12">
-		                        	<textarea class="form-control" name="gender" placeholder="Input any descriptions" required></textarea>
+		                        	<textarea class="form-control" name="gender" placeholder="Gender" required></textarea>
 		                        </div>
 		                    </div>
 		                    <div class="form-group">
-		                        <label class="col-lg-2 col-sm-2 control-label">Birth Date</label>
+		                        <label class="col-lg-4 col-sm-4 control-label">Birth Date</label>
 		                        <div class="col-lg-12">
-		                        	<textarea class="form-control" name="birth" placeholder="Input any descriptions" required></textarea>
+		                        	<input type="date" class="form-control" name="birth" required>
 		                        </div>
 		                    </div>
 		                    <div class="form-group">
-		                        <label class="col-lg-2 col-sm-2 control-label">Picture</label>
+		                        <label class="col-lg-4 col-sm-4 control-label">Picture</label>
 		                        <div class="col-lg-12">
 		                            <input type="file" class="form-control" name="pic" required>
 		                        </div>
@@ -136,26 +126,26 @@
 	             <?php echo form_open_multipart('EventArtist/update'); ?>
 		            <div class="modal-body">
 		                    <div class="form-group">
-		                        <label class="col-lg-2 col-sm-2 control-label">Name</label>
+		                        <label class="col-lg-4 col-sm-4 control-label">Name</label>
 		                        <div class="col-lg-12">
 		                        	<input type="hidden" id="id" name="id">
-		                            <input type="text" class="form-control" id="name" name="name" placeholder="Input Event Name" required>
+		                            <input type="text" class="form-control" id="name" name="name" placeholder="Input Artist Name" required>
 		                        </div>
 		                    </div>
 		                    <div class="form-group">
-		                        <label class="col-lg-2 col-sm-2 control-label">Gender</label>
+		                        <label class="col-lg-4 col-sm-4 control-label">Gender</label>
 		                        <div class="col-lg-12">
-		                        	<textarea class="form-control" id="gender" name="gender" placeholder="Input any descriptions" required></textarea>
+		                        	<textarea class="form-control" id="gender" name="gender" placeholder="Gender" required></textarea>
 		                        </div>
 		                    </div>
 		                     <div class="form-group">
-		                        <label class="col-lg-2 col-sm-2 control-label">Birth Date</label>
+		                        <label class="col-lg-4 col-sm-4 control-label">Birth Date</label>
 		                        <div class="col-lg-12">
-		                        	<textarea class="form-control" id="birth" name="birth" placeholder="Input any descriptions" required></textarea>
+		                        	<input type="date" class="form-control" id="birth" name="birth" required>
 		                        </div>
 		                    </div>
 		                    <div class="form-group">
-		                        <label class="col-lg-2 col-sm-2 control-label">Photo</label>
+		                        <label class="col-lg-4 col-sm-4 control-label">Photo</label>
 		                        <div class="col-lg-12">
 		                            <input type="file" class="form-control" id="pic" name="pic">
 		                        </div>
@@ -182,7 +172,7 @@
 	            modal.find('#id').attr("value",div.data('id'));
 	            modal.find('#name').attr("value",div.data('name'));
 	            modal.find('#gender').html(div.data('gender'));
-	             modal.find('#birth').html(div.data('birth'));
+	            modal.find('#birth').attr("value",div.data('birth'));
 	            modal.find('#pic').attr("value",div.data('pic'));
 	        });
 	    });
