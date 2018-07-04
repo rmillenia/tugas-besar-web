0840462b4778d724db13d58ec2d5f8bb1fae5723
rmillenia <35435291+rmillenia@users.noreply.github.com>
2018-07-04 17:44:36 +0700

    controller Admin Detail

--- application/controllers/AdminDetail.php
+++ application/controllers/AdminDetail.php
@@ -12,11 +12,8 @@ class AdminDetail extends CI_Controller {
 
         $this->load->model('user');
         $id = $data['id'];
-        $user = $data['username'];
         $data['name'] = $this->user->selectAll($id,$user);
 
-		$this->load->model('InputAdminModel');
-        $data["admin_list"] = $this->InputAdminModel->getAdmin();
         $this->load->view('admin/header', $data);
         $this->load->view('admin/sidebar'); 
         $this->load->view('admin/profileAdmin', $data);
@@ -33,11 +30,6 @@ class AdminDetail extends CI_Controller {
         $data['id']=$session_data['id'];
         $id = $data['id'];
         
-
-        if ($this->form_validation->run()==FALSE){
-
-        }else{
-
             $config['upload_path']='./assets/imgEvent/';
             $config['allowed_types']='gif|jpg|png';
             $config['max_size']=1000000000;
@@ -55,29 +47,61 @@ class AdminDetail extends CI_Controller {
                 redirect('AdminDetail','refresh');
         	}
     	}
-	}
 
 		public function update()
-	{
-		$this->load->model('InputAdminModel');
+	   {
 
 		$session_data=$this->session->userdata('logged_in');
         $data['username']=$session_data['username'];
         $data['level']=$session_data['level'];
         $data['id']=$session_data['id'];
+
         $id = $data['id'];
 
+        $this->load->model('InputAdminModel');
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('name', 'Full Name', 'trim|required');
+        $this->form_validation->set_rules('add', 'Address', 'trim|required');
+        $this->form_validation->set_rules('phone', 'Phone Number', 'trim|required');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required');
+        $this->form_validation->set_rules('username', 'Username', 'trim|required');
+
+
+        if ($this->form_validation->run() == FALSE) {
+            $session_data=$this->session->userdata('logged_in');
+        $data['username']=$session_data['username'];
+        $data['level']=$session_data['level'];
+        $data['id']=$session_data['id'];
+
+        $this->load->model('user');
+        $id = $data['id'];
+        $data['name'] = $this->user->selectAll($id);
+
+        $this->load->model('InputAdminModel');
+        $data["admin_list"] = $this->InputAdminModel->getAdmin();
+        $this->load->view('admin/header', $data);
+        $this->load->view('admin/sidebar'); 
+        $this->load->view('admin/profileAdmin', $data);
+            
+	   } else {
         $pass = $this->input->post('password');
-        if(empty($pass)){
-        	$this->InputAdminModel->updateNoPass($id);
-			echo "<script>alert('Successfully Updated'); </script>";
-			redirect('AdminDetail','refresh');
-        }else{
-        	$this->InputAdminModel->updateProfile($id);
-			echo "<script>alert('Successfully Updated'); </script>";
-			redirect('AdminDetail','refresh');
+            if(empty($pass)){
+               $this->InputAdminModel->updateNoPass($id);
+                $session_data['username'] = $this->input->post('username');
+                $data['username']=$session_data['username'];
+                 echo "<script>alert('Successfully Updated'); </script>";
+                 redirect('AdminDetail','refresh');
+            }else{
+               $this->InputAdminModel->updateProfile($id);
+                $session_data['username'] = $this->input->post('username');
+                $session_data['password'] = MD5($this->input->post('password'));
+                $data['username']=$session_data['username'];
+                $data['password']=$session_data['password'];
+                  echo "<script>alert('Successfully Updated'); </script>";
+                 redirect('AdminDetail','refresh');
+            }
         }
-	}
+    }
 
 }
 
